bd5955552ce7da333472d70826383a8eab1bc2a9
the7winds <vga.grc@gmail.com>
2016-09-17 14:04:49 +0300

    added first part of task

---
+++ script.sql
@@ -0,0 +1,60 @@
+-- all about cures
+
+CREATE TABLE Cure(
+    id INT
+        CHECK (id >= 0),
+    name TEXT
+        NOT NULL,
+    internationalName TEXT
+        NOT NULL,
+    cureFormId INT
+        CHECK (cureFormId >= 0),
+    manufactorerId INT
+        CHECK (manufactorerId >= 0),
+    componentId INT
+        CHECK (manufactorerId >= 0),
+    certificateId INT
+        CHECK (manufactorerId >= 0),
+    UNIQUE(id));
+
+CREATE TABLE CureForm(
+    id INT
+        CHECK (id >= 0),
+    name TEXT
+        NOT NULL,
+    UNIQUE(id));
+
+CREATE TABLE Manufactorer(
+    id INT
+        CHECK (id >= 0),
+    name TEXT
+        NOT NULL,
+    UNIQUE(id));
+
+CREATE TABLE Component(
+    id INT
+        CHECK (id >= 0),
+    name TEXT
+        NOT NULL,
+    formula TEXT
+        NOT NULL,
+    UNIQUE(id));
+
+CREATE TABLE Certificates(
+    id INT
+        CHECK (id >= 0),
+    expires DATE
+        NOT NULL,
+    labId INT
+        CHECK (id >= 0),
+    UNIQUE(id));
+
+CREATE TABLE Lab(
+    id INT
+        CHECK (id >= 0),
+    labName TEXT
+        NOT NULL,
+    boss TEXT
+        NOT NULL,
+    UNIQUE(id));
+

3d68bea58f0883377eb4baed29b1ea1656df2f74
the7winds <vga.grc@gmail.com>
2016-09-17 20:08:09 +0300

    change: UNIQUE -> PRIMARY KEY

--- script.sql
+++ script.sql
@@ -2,7 +2,8 @@
 
 CREATE TABLE Cure(
     id INT
-        CHECK (id >= 0),
+        CHECK (id >= 0)
+        PRIMARY KEY,
     name TEXT
         NOT NULL,
     internationalName TEXT
@@ -14,53 +15,52 @@ CREATE TABLE Cure(
     componentId INT
         CHECK (componentId >= 0),
     certificateId INT
-        CHECK (certificateId >= 0),
-    UNIQUE(id)
+        CHECK (certificateId >= 0)
 );
 
 CREATE TABLE CureForm(
     id INT
-        CHECK (id >= 0),
+        CHECK (id >= 0)
+        PRIMARY KEY,
     name TEXT
-        NOT NULL,
-    UNIQUE(id)
+        NOT NULL
 );
 
 CREATE TABLE Manufactorer(
     id INT
-        CHECK (id >= 0),
+        CHECK (id >= 0)
+        PRIMARY KEY,
     name TEXT
-        NOT NULL,
-    UNIQUE(id)
+        NOT NULL
 );
 
 CREATE TABLE Component(
     id INT
-        CHECK (id >= 0),
+        CHECK (id >= 0)
+        PRIMARY KEY,
     name TEXT
         NOT NULL,
     formula TEXT
-        NOT NULL,
-    UNIQUE(id)
+        NOT NULL
 );
 
 CREATE TABLE Certificates(
     id INT
-        CHECK (id >= 0),
+        CHECK (id >= 0)
+        PRIMARY KEY,
     expires DATE
         NOT NULL,
     labId INT
-        CHECK (id >= 0),
-    UNIQUE(id)
+        CHECK (id >= 0)
 );
 
 CREATE TABLE Lab(
     id INT
-        CHECK (id >= 0),
+        CHECK (id >= 0)
+        PRIMARY KEY,
     labName TEXT
         NOT NULL,
     boss TEXT
-        NOT NULL,
-    UNIQUE(id)
+        NOT NULL
 );
 

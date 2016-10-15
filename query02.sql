17ab6a6e17fd1738a0fba7aab816c06106e673c8
Olga Chernikova <Oche24912@yandex.ru>
2016-10-15 14:43:21 +0300

    task 2

---
+++ query02.sql
@@ -0,0 +1,6 @@
+SELECT Laboratory.id, Laboratory.name
+    FROM Laboratory 
+        JOIN Drug ON Laboratory.id = Drug.certificateLaboratoryId
+        JOIN Component ON Component.id = Drug.componentId
+    WHERE 
+        Component.name = 'aqua pura';

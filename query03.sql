9396fb06f55fea6010bd98e6cedfffb54a664ec2
the7winds <vga.grc@gmail.com>
2016-10-15 18:21:55 +0300

    many attributes -> Drug.* added comment

--- query03.sql
+++ query03.sql
@@ -1,12 +1,8 @@
-SELECT Drug.id,
-       Drug.name,
-       Drug.internationalName,
-       Drug.dosageFormId,
-       Drug.manufacturerId,
-       Drug.componentId,
-       Drug.certificateId,
-       Drug.certificateExpires,
-       Drug.certificateLaboratoryId
+-- We must get drugs wich don't have records in DrugsInDrugstore table or have zero
+-- amount of the drugs (because DrugsInDrugstore.itemsCount is NOT NULL CHECK >= 0).
+-- That's why we use left join.
+
+SELECT Drug.*
     FROM Drug
         LEFT JOIN DrugsInDrugstore AS Store ON Drug.id = Store.drugId AND Store.itemsCount > 0
     WHERE Store.drugId IS NULL;

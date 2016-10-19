bc0434164d3ba8fe160ffb6e706653d487bf350c
the7winds <vga.grc@gmail.com>
2016-10-19 17:09:17 +0300

    removed useless ELSE

--- query07.sql
+++ query07.sql
@@ -7,11 +7,11 @@ SELECT Drug.name,
        Drugstore.number AS drugStoreNumber,
        DnD.price, 
        CASE WHEN DnD.price = MNXP.mxPrice AND DnD.price = MNXP.mnPrice THEN 'min=max'
-        ELSE CASE WHEN DnD.price = MNXP.mxPrice THEN 'max'
-                  WHEN DnD.price = MNXP.mnPrice THEN 'min' END END AS indicator
+            WHEN DnD.price = MNXP.mxPrice THEN 'max'
+            WHEN DnD.price = MNXP.mnPrice THEN 'min' END AS indicator
     FROM Drug 
         JOIN DrugsInDrugstore AS DnD ON Drug.id = DnD.drugId
         JOIN Drugstore ON DnD.drugstoreId = Drugstore.id
         JOIN MinMaxPrice AS MNXP ON 
-            Drug.name = MNXP.name;-- AND (price = mxPrice OR price = mnPrice);
+            Drug.name = MNXP.name;
      

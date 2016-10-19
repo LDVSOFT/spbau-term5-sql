bf358ab31daa44b1b890c085ed0939e264a90917
LDVSOFT <lapshin.dv@mail.ru>
2016-10-19 17:01:25 +0300

    Fixed 5 & 8

--- query05.sql
+++ query05.sql
@@ -1,4 +1,5 @@
 SELECT
 	D.id, MIN(DID.price), AVG(DID.price), MAX(DID.price)
 	FROM Drugstore D
-	LEFT JOIN DrugsInDrugstore DID ON D.id = DID.drugstoreId;
+	LEFT JOIN DrugsInDrugstore DID ON D.id = DID.drugstoreId
+	GROUP BY D.id;
--- query08.sql
+++ query08.sql
@@ -1,7 +1,7 @@
 SELECT
 	D.*,
-	SUM(WhDP.itemPurchasePrice * WhDP.itemsPerPackage * WhDP.packageCount)
+	SUM(CASE WHEN WhD.id IS NOT NULL THEN WhDP.itemPurchasePrice * WhDP.itemsPerPackage * WhDP.packageCount ELSE 0 END)
 	FROM WarehouseDeliveryPart WhDP
 		JOIN WarehouseDelivery WhD ON WhD.id = WhDP.deliveryId
-		JOIN Distributor D ON D.id = WhD.distributorId
+		RIGHT JOIN Distributor D ON D.id = WhD.distributorId
 	GROUP BY D.id;

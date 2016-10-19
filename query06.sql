82ea7be8e83d4a68802337e4d9d4c99f77f0936d
LDVSOFT <lapshin.dv@mail.ru>
2016-10-19 17:09:36 +0300

    Fixed 6

--- query06.sql
+++ query06.sql
@@ -1,3 +1,3 @@
-SELECT Drug.id, Drug.name, SUM(DrugsInDrugstore.itemsCount) 
+SELECT Drug.id, Drug.name, COALESCE(SUM(DrugsInDrugstore.itemsCount), 0)
     FROM Drug LEFT JOIN DrugsInDrugstore ON Drug.id = DrugsInDrugstore.drugId
     GROUP BY Drug.id;

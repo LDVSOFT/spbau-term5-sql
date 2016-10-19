b13ab0a5e92ff557037d005f9757259e0c764cb5
the7winds <vga.grc@gmail.com>
2016-10-19 18:16:34 +0300

    #1 removed distinct

--- problem01.sql
+++ problem01.sql
@@ -1,7 +1,7 @@
 -- Для каждой расы найти планету, куда летит больше всего
 
 WITH PlanetCounter AS (
-    SELECT DISTINCT ON (race, count) Pax.race, Planet.name AS planet, Count(*) AS count
+    SELECT Pax.race, Planet.name AS planet, Count(*) AS count
         FROM Pax JOIN Booking ON Pax.id = Booking.pax_id
              JOIN Flight ON Booking.flight_id = Flight.id
              JOIN Planet ON Planet.id = Flight.planet_id
@@ -10,8 +10,7 @@ WITH PlanetCounter AS (
 SELECT race, planet, count
     FROM PlanetCounter AS PC1
     WHERE count IN (
-        SELECT count
+        SELECT MAX(count)
             FROM PlanetCounter AS PC2
             WHERE PC1.race = PC2.race
-            ORDER BY count DESC LIMIT 1
     );

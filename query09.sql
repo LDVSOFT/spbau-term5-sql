b9c6c477f33f0671d104b3979d4c035fcb71ae45
the7winds <vga.grc@gmail.com>
2016-11-16 15:23:14 +0300

    optimized query 09

--- query09.sql
+++ query09.sql
@@ -1,25 +1,24 @@
 WITH
-	DeliveryCount AS (
-		SELECT
-			DsDP.drugstoreId,
-			DsD.warehouseId,
-			COUNT(DISTINCT DsD.id) AS deliveryCount
-			FROM DrugstoreDeliveryPart DsDP
-				JOIN DrugstoreDelivery DsD ON DsD.id = DsDP.deliveryId
-			GROUP BY 1, 2
-	)
+    DeliveryCount AS (
+        SELECT
+            DsDP.drugstoreId,
+            DsD.warehouseId,
+            COUNT(DISTINCT DsD.id) AS deliveryCount
+            FROM DrugstoreDeliveryPart DsDP
+                JOIN DrugstoreDelivery DsD ON DsD.id = DsDP.deliveryId
+            GROUP BY 1, 2
+    ),
+    Tmp AS (
+        SELECT
+            Ds.number,
+            Wh.number,
+            DC.deliveryCount,
+            RANK() OVER (PARTITION BY Ds.number ORDER BY DC.deliveryCount DESC)
+        FROM DeliveryCount DC
+            JOIN Drugstore Ds ON Ds.id = DC.drugstoreId
+            JOIN Warehouse Wh ON Wh.id = DC.warehouseId
+    )
 SELECT
-	Ds.number,
-	Wh.number,
-	DC.deliveryCount
-	FROM DeliveryCount DC
-		JOIN Drugstore Ds ON Ds.id = DC.drugstoreId
-		JOIN Warehouse Wh ON Wh.id = DC.warehouseId
-	WHERE
-		Wh.id = ANY (
-			SELECT DC2.warehouseId
-				FROM DeliveryCount DC2
-				WHERE DC2.drugstoreId = DC.drugstoreId
-				ORDER BY DC2.deliveryCount DESC
-				LIMIT 2
-		);
+    *
+    FROM TMP
+    WHERE rank <= 2;
